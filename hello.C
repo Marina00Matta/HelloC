5153f19aa57f15e656763c7640e11fd2a902254a
mmatta@win10 <marinamedhat_19@hotmail.com>
2019-10-11 21:39:33 +0200

    adding source code of hello world khara file in c

---
+++ hello.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int x,y;
+printf("enter first number\n");
+scanf("%d",&x);
+printf("enter second number\n");
+scanf("%d",&y);
+printf("the sum is %d,x+y");
+getch();
+return 0;
+}
